e862c72433ac2906469aeeef487d176d0dc26850
Kevin Turner <kevinturner@utexas.edu>
2018-11-20 15:55:50 -0600

    Initial commit of Tock Pi iPhone client.

--- Tock Pi Client/ViewController.swift
+++ Tock Pi Client/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alarmTimePicker: UIDatePicker!
+    @IBOutlet weak var ipAddressInput: UITextField!
+    private var isConnected : Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.alarmTimePicker.isEnabled = false
+        let rpiAddress = UserDefaults.standard.string(forKey: "address") ?? "127.0.0.1"
+        connectRpi(atAddress: rpiAddress)
+    }
+    
+    private func connectRpi(atAddress rpiAddress: String) {
+        Alamofire.request("http://\(rpiAddress)/connect").responseJSON {response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+        }
     }
 
-
+    @IBAction func connectButtonPressed(_ sender: Any) {
+        print("connect button pressed")
+        let rpiAddress = self.ipAddressInput.text! // Yuck! Fix this
+        connectRpi(atAddress: rpiAddress)
+        
+    }
+    
 }
 
